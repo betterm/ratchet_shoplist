fa745189e59dfec9da691de7c9dccaaa5787acf6
Misha <mishakib91@icloud.com>
2015-12-01 01:48:31 +0200

    ratcher-sass gem

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,6 @@ module RatchetShoplist
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
-    config.assets.paths << Rails.root.join("app", "assets", "examples")
+    config.assets.precompile += %w( *.svg *.eot *.woff *.ttf )
   end
 end
